b116582cb60ab1f76cbe456ba0a55ad0e60e7ff4
Thomas Truong <thomas.truong1@icloud.com>
2025-11-28 17:57:04 +0100

    fix: Update Stripe API version to match installed package

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? 'sk_test_placeholder', {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2024-12-18.acacia',
   typescript: true,
 });
 
